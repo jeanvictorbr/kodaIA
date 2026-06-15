f330b43bbbec14f102a11b6e523e46ea5539e626
jeanvictorbr <jeanvictorbr@gmail.com>
2026-06-15 01:30:53 -0300

    Update prisma.js

--- src/database/prisma.js
+++ src/database/prisma.js
@@ -1,5 +1,8 @@
 // src/database/prisma.js
-import { PrismaClient } from '@prisma/client';
+
+// Importação compatível com ES Modules e CommonJS
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
 
 // Padrão Singleton: Evita abrir centenas de conexões se o bot reiniciar rápido
 const prisma = global.prisma || new PrismaClient();
